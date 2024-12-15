3ab43894d756aa2c46c9fe13d7a473d4465abe75
Tehreem <tehreemriaz87@gmail.com>
2024-12-15 16:25:23 +0500

    integrated api with recommended news

--- src/components/Recommended.tsx
+++ src/components/Recommended.tsx
@@ -1,29 +1,35 @@
-import { getNews } from "@/api/news";
-
+import { getRecentNews } from "@/api/news";
+import { get } from "http";
 export default async function Recommended() {
+  const news = await getRecentNews();
 
-    const news = await getNews();
-  
-    return (
-      <section className="py-10" >
-        <div className="container mx-auto">
-          <h2 className="text-xl font-bold mb-6">Recommended</h2>
-          <div className="grid grid-cols-1 gap-1">
-            {news.map(({ title, summaryText, imagelink, _id }) => (
-              <div className="bg-white p-4 flex" key={_id}>
-                <img
-                  src={imagelink}
-                  alt="News"
-                  className="w-20 h-20 object-cover"
-                />
-                <div className="ml-4">
-                  <h3 className="text-sm font-bold">{title}</h3>
-                  <p className="text-sm text-ellipsis">{summaryText}</p>
-                </div>
+  return (
+    <section className="py-10">
+      <div className="mx-auto">
+        <h2 className="text-xl font-bold mb-6">Recommended</h2>
+        <div className="grid grid-cols-1 gap-1">
+          {news.map(({ title, short_summary, imageUrl, _id }) => (
+            <div className="bg-white p-4 flex" key={_id}>
+              <img
+                src={imageUrl}
+                alt="News"
+                className="w-20 h-20 object-cover"
+              />
+              <div
+                className="ml-4 text-ellipsis overflow-hidden"
+                style={{
+                  display: "-webkit-box",
+                  WebkitLineClamp: 3, // Limit to 3 lines total
+                  WebkitBoxOrient: "vertical",
+                }}
+              >
+                <span className="text-sm font-bold">{title}</span>
+                <span className="text-xs"><br></br> {short_summary}</span>
               </div>
-            ))}
             </div>
+          ))}
         </div>
-      </section>
-    );
-} 
+      </div>
+    </section>
+  );
+}
