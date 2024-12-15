9110443aa90ad2596780cec1b25cb31d3e361000
Tehreem <tehreemriaz87@gmail.com>
2024-12-15 19:32:31 +0500

    added getNewsById api

--- src/api/news.ts
+++ src/api/news.ts
@@ -1,19 +1,25 @@
-export interface INewsCard {
+interface INews {
   _id: string;
-  url?: string;
-  imageUrl: string;
-  source?: string;
-  content?: string;
   title: string;
-  datetime?: string
-  short_summary?: string;
-  long_summary?: string;  
+  short_summary: string;
+  long_summary: string;
+  imageUrl: string;
+  source: string;
+  date_time: string;
 }
 
-export const getRecentNews = async (): Promise<INewsCard[]> => {
+export const getRecentNews = async (): Promise<INews[]> => {
   const response= await fetch('http://16.171.28.118:8000/recent-news/?count=15', {
     cache: 'no-store', // Avoids caching for dynamic data
   });
   const data = await response.json();
   return data?.recent_news;
+}
+
+export const getNewsById = async (id: string): Promise<INews> => {
+  const response= await fetch(`http://16.171.28.118:8000/news/${id}`, {
+    cache: 'no-store', // Avoids caching for dynamic data
+  });
+  const data = await response.json();
+  return data;
 }
