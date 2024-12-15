3fa61738e19c348b28598cc748143cf239faeef5
Tehreem <tehreemriaz87@gmail.com>
2024-12-15 19:34:41 +0500

    integrated news data with article page

--- src/components/ArticleDetails.tsx
+++ src/components/ArticleDetails.tsx
@@ -1,43 +1,32 @@
 import React from "react";
-import { getNews } from "@/api/news";
-const Article = async () => {
-  const news = await getNews();
+import { getNewsById } from "@/api/news";
+
+interface ArticleProps {
+  id: string;
+}
+
+const Article = async ({ id }: ArticleProps) => {
+  const news = await getNewsById(id);
+  const { _id, title, imageUrl, long_summary, source, date_time } = news;
   return (
     <div className=" bg-gray-50">
       <div className="max-w-4xl mx-auto bg-white shadow-md overflow-hidden">
-        {/* Article Header */}
         <div className="relative">
           <img
-            src={news[2]["imagelink"]}
+            src={imageUrl}
             alt="Article"
             className="w-full h-64 object-cover"
           />
         </div>
-
-        {/* Article Content */}
         <div className="p-6">
           <div>
-            <h1 className="text-2xl text-black font-bold mb-3">
-              {news[2]["title"]}
-            </h1>
+            <h1 className="text-2xl text-black font-bold mb-3">{title}</h1>
           </div>
           <div className="mb-4 text-gray-500 text-sm flex justify-between">
-            <span>Author: Jane Doe</span>
-            <span>Published: November 24, 2024</span>
+            <span>{source}</span>
+            <span>{date_time && new Date(date_time).toLocaleString()}</span>
           </div>
-          <p className="text-gray-700 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            vitae congue ligula. Curabitur vel bibendum ligula. Fusce feugiat,
-            quam id dapibus convallis, erat turpis cursus metus, nec luctus ex
-            ante ac nulla. Sed tristique, eros nec aliquet ultrices, felis sem
-            euismod urna, nec laoreet eros nunc nec augue.
-          </p>
-          <p className="mt-4 text-gray-700 leading-relaxed">
-            Proin luctus dolor non augue dictum, non vulputate elit suscipit.
-            Morbi sit amet sagittis dolor. Duis vestibulum nisi eu tortor
-            suscipit, non faucibus risus vulputate. Suspendisse vel quam nec
-            nulla commodo cursus.
-          </p>
+          <p className="text-gray-700 leading-relaxed">{long_summary}</p>
         </div>
 
         {/* Tags */}
